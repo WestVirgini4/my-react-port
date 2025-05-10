94dce2bf7dce6003b50c56b611b4d23a2d487186
WestVirgini4 <yourufutureboyfriends27@gmail.com>
2025-05-10 13:15:59 +0700

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 const Header = () => {
+    /*==============toggle===============*/ 
+    const[Toggle, showMenu] = useState(false);
   return (
     <header className='header'>
         <nav className="nav container">
             <a href="index" className='nav__logo'>Korramis</a>
 
-            <div className="nav__menu">
+            <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                 <ul className="nav__list grid">
                     <li className="nav__item">
                         <a href="#home" className="nav__link active-link">
@@ -45,9 +47,9 @@ const Header = () => {
                     </li>
                 </ul>
 
-                <i class="uil uil-times nav__close"></i>
+                <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
             </div>
-            <div className="nav__toggle">
+            <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
                 <i class="uil uil-apps"></i>
             </div>
         </nav>
